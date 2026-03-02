6b4c67d75938b77a5b31fcd3da2233ab642e9eac
Vandana yadav <vandanayadav202004@gmail.com>
2026-03-02 17:10:02 +0530

    added code

--- learning_platform/app/urls.py
+++ learning_platform/app/urls.py
@@ -3,8 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/',views.about,name="about") ,
-    path('course/',views.courses,name="course"),
-    path('contact/',views.contact,name="contact"),
-]
+    path('about/', views.about, name="about"),
+    path('course/', views.courses, name="course"),
+    path('contact/', views.contact, name="contact"),
 
+    # Authentication
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+    path('reset/<uidb64>/<token>/', views.reset_password, name='reset_password'),
+]
